69c374802aa5b15451aeb6a36788679eec0ce3a7
jrpompio <junior.ruiz@ucr.ac.cr>
2024-05-06 04:04:52 -0600

    YA FUNCIONA, solo ff no
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

---
+++ prueba/testbench.v
@@ -0,0 +1,33 @@
+`include "PM_synth.v"
+//`include "parkingmanager.v"
+`include "tester.v"
+`include "cmos_cells.v"
+
+module testbench;
+
+    wire clk, sensorA, sensorB,
+    gateState, blockAlarm, wrongPinAlarm; 
+    wire [7:0] pass;
+    
+test source(
+    .clk(clk),
+    .sensorA(sensorA),
+    .sensorB(sensorB),
+    .pass(pass),
+    .gateState(gateState),
+    .blockAlarm(blockAlarm),
+    .wrongPinAlarm(wrongPinAlarm)
+);
+
+parkingmanager Uutmain(
+    .clk(clk),
+    .sensorA(sensorA),
+    .sensorB(sensorB),
+    .pass(pass),
+    .gateState(gateState),
+    .blockAlarm(blockAlarm),
+    .wrongPinAlarm(wrongPinAlarm)
+);
+
+
+endmodule
